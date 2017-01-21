a3d5947453952188fe769df0e396967099200f49
Zhipeng Liu <hustlzp@qq.com>
2017-01-21 11:31:09 +0800

    Update UIColor+Helper.swift

--- Extensions/UIColor+Helper.swift
+++ Extensions/UIColor+Helper.swift
@@ -10,8 +10,21 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    class func colorWithRGBA(color: Int64) -> UIColor {
-        return UIColor(red: (CGFloat)((color >> 24) & 0xFF) / 255.0, green: (CGFloat)((color >> 16) & 0xFF) / 255.0, blue: (CGFloat)((color >> 8) & 0xFF) / 255.0, alpha: (CGFloat)((color) & 0xFF) / 255.0);
+    convenience init(hex: Int64) {
+        self.init(red: (CGFloat)((hex >> 24) & 0xFF) / 255.0, green: (CGFloat)((hex >> 16) & 0xFF) / 255.0, blue: (CGFloat)((hex >> 8) & 0xFF) / 255.0, alpha: (CGFloat)((hex) & 0xFF) / 255.0)
+    }
+    
+    convenience init(hexString: String) {
+        var cString:String = hexString.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        var rgbValue: UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0, green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0, blue: CGFloat(rgbValue & 0x0000FF) / 255.0, alpha: CGFloat(1.0))
     }
     
     func lighter(percentage: CGFloat) -> UIColor {
@@ -27,4 +40,5 @@ extension UIColor {
         getHue(&h, saturation: &s, brightness: &b, alpha: &a)
         return UIColor(hue: h, saturation: s, brightness: min(b * (1 - percentage), 1), alpha: a)
     }
+
 }
