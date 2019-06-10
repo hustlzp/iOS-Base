c2170219a08c1edab4d9bb2dcddc8c8a7cd793c7
Zhipeng Liu <hustlzp@qq.com>
2019-06-10 17:35:36 +0800

    Update Constants.swift

--- Constants.swift
+++ Constants.swift
@@ -84,4 +84,16 @@ struct Device {
     static var isVertical: Bool {
         return Size.screenWidth < Size.screenHeight
     }
+    
+    static var modelName: String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        
+        return identifier
+    }
 }
