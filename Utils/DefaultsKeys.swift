21e02b7c094e23b9acd0d031929c5c7b046dbbec
Zhipeng Liu <hustlzp@qq.com>
2019-06-10 17:37:52 +0800

    Create DefaultsKeys.swift

---
+++ Utils/DefaultsKeys.swift
@@ -0,0 +1,14 @@
+//
+//  DefaultsKeys.swift
+//  iOS-Base
+//
+//  Created by hustlzp on 2019/6/9.
+//  Copyright © 2019 hustlzp. All rights reserved.
+//
+
+import Foundation
+import SwiftyUserDefaults
+
+extension DefaultsKeys {
+    static let didShowStoreReviewController = DefaultsKey<Bool>("didShowStoreReviewController_2019_06", defaultValue: false)    // 弹窗好评
+}
