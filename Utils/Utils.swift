d16317668b0b7950db097f3dc63c8154a46a87db
Zhipeng Liu <hustlzp@qq.com>
2019-06-10 17:36:38 +0800

    Update Utils.swift

--- Utils/Utils.swift
+++ Utils/Utils.swift
@@ -1,16 +1,17 @@
 import UIKit
 
 class Utils: NSObject {
-    class func dispatchAfter(second: Double, completion: (() -> Void)?) {
+    class func dispatchAfter(_ second: Double, completion: (() -> Void)?) {
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (Int64)(second * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
             completion?()
         }
     }
 
-    class func dispatchInMainQueue(completion: () -> Void) {
-        dispatch_async(dispatch_get_main_queue()) {
-            () -> Void in
-            completion()
-        }
+    class func generateImpactFeedback(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        
+        generator.prepare()
+        generator.impactOccurred()
     }
+
 }
