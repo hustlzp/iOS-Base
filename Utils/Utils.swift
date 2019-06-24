68c553e26a5b14f551f8f73d80ac64cf24325e04
Zhipeng Liu <hustlzp@qq.com>
2019-06-24 18:20:04 +0800

    Update Utils.swift

--- Utils/Utils.swift
+++ Utils/Utils.swift
@@ -10,8 +10,54 @@ class Utils: NSObject {
     class func generateImpactFeedback(style: UIImpactFeedbackGenerator.FeedbackStyle) {
         let generator = UIImpactFeedbackGenerator(style: style)
         
-        generator.prepare()
         generator.impactOccurred()
+    }
+    
+    // MARK: Permissions
+    
+    class func checkCameraAuthDenied() -> Bool {
+        return AVCaptureDevice.authorizationStatus(for: AVMediaType.video) == .denied
+    }
+    
+    class func checkAlbumAuthDenied() -> Bool {
+        return PHPhotoLibrary.authorizationStatus() == .denied
+    }
+    
+    class func checkMicrophoneAuthDenied() -> Bool {
+        return AVAudioSession.sharedInstance().recordPermission == .denied
+    }
+    
+    class func checkRegisteredForRemoteNotifications() -> Bool {
+        return UIApplication.shared.isRegisteredForRemoteNotifications
+    }
+    
+    class func openSettingsViewController() {
+        guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
+        
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+    // MARK: Show permission request alert
+    
+    class func showPermissionReuqestAlert(_ controller: UIViewController, title: String) {
+        let alertController = UIAlertController(title: nil, message: title, preferredStyle: .alert)
+        alertController.addAction("取消".localized(), style: .cancel)
+        alertController.addAction("去设置".localized(), style: .default, handle: { (action) -> Void in
+            self.openSettingsViewController()
+        })
+        controller.present(alertController, animated: true, completion: nil)
+    }
+    
+    class func showAlbumPermissionReuqestAlert(_ controller: UIViewController) {
+        showPermissionReuqestAlert(controller, title: "请允许XXX访问您的照片".localized())
+    }
+    
+    class func showCameraPermissionReuqestAlert(_ controller: UIViewController) {
+        showPermissionReuqestAlert(controller, title: "请允许XXX使用您的相机".localized())
+    }
+    
+    class func showMicrophonePermissionRequestAlert(_ controller: UIViewController) {
+        showPermissionReuqestAlert(controller, title: "请允许XXX使用您的麦克风".localized())
     }
 
 }
